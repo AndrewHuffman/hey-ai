2f330f25ce9cd82c49f503538cc5bb196183b72f
Andrew Huffman <andrewhuffman@gmail.com>
2026-08-25 20:01:34 -0400

    fix(package): strip npm credentials case-insensitively

--- scripts/npm-environment.mjs
+++ scripts/npm-environment.mjs
@@ -5,13 +5,11 @@ export function createIsolatedNpmEnvironment(baseEnvironment, cacheDirectory, us
   };
 
   for (const environmentName of Object.keys(environment)) {
-    if (/^npm_config_/i.test(environmentName)) {
+    if (/^(?:npm_config_.*|node_auth_token|npm_token)$/i.test(environmentName)) {
       delete environment[environmentName];
     }
   }
 
-  delete environment.NODE_AUTH_TOKEN;
-  delete environment.NPM_TOKEN;
   environment.NPM_CONFIG_CACHE = cacheDirectory;
   environment.NPM_CONFIG_USERCONFIG = userConfigPath;
   return environment;
